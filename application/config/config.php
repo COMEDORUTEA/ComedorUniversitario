fe1d98a408a207b82fbf4b8176dc59bf41d312f6
RubenTorres Bazan <39686493+rubentb@users.noreply.github.com>
2018-05-28 15:13:06 -0500

    configurando http://localhost:8080/comedor
    $config['base_url']	= 'http://localhost:8080/comedor';

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:888/egresados';
+$config['base_url']	= 'http://localhost:8080/comedor';
 
 /*
 |--------------------------------------------------------------------------
