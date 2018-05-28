39463c62c3143b5b1f4b101c1a76353f7ff87219
RubenTorres Bazan <39686493+rubentb@users.noreply.github.com>
2018-05-28 15:08:44 -0500

    La contraseña de  http://localhost:8080/phpmyadmin
    $db['default']['password'] = 'usbw';

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'usbw';
 $db['default']['database'] = 'dbencuesta';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
