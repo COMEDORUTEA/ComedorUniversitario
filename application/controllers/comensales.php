8e2fb7e74b346c81c1222ecd3ce56bd139701170
RubenTorres Bazan <39686493+rubentb@users.noreply.github.com>
2018-05-29 16:27:35 -0500

    creando controller comensales
    <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
    date_default_timezone_set('America/Mexico_City'); 
    class comensales extends CI_Controller
    {
    
         public function __construct()
         {
              parent::__construct();
              //Cargamos el modelo deel controlador
              $this->load->model('model_usuarios');
              $this->load->model('model_seguridad');
              $this->load->model('model_login');
              $this->load->model('model_comensales');
         }
         function Seguridad(){
         	$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
             $this->model_seguridad->SessionActivo($url);
         }
         public function index(){
              /*Si el usuario esta logeado*/
              $this->Seguridad();
              $this->load->view('header');
              $data['comensales'] = $this->model_comensales->ListarComensales();         
              $this->load->view('view_comensales', $data);
              $this->load->view('footer');
    	}
    	public function nuevo(){
    	      
            /*Si el usuario esta logeado*/
            $this->Seguridad();
    		$hoy    = date("Y")."-".date("m")."-".date("d")." ".date("H:i:s");
    				
    		$this->ValidaCampos();
    		if($this->form_validation->run() == TRUE){
    				//Verificamos si existe el CODIGO
    			   $VerifyExist = $this->model_comensales->ExisteCodigo($this->input->post("CODIGO"));
                   if($VerifyExist==0){
                   	    $ComensalInsertar = $this->input->post();//Recibimos todo los campos por array nos lo envia codeigther
                   	    $ComensalInsertar["FECHA_REGISTRO"] = $hoy;//le agregamos la fecha de registro
                   	    //guardamos los registros
                   	    $this->model_comensales->SaveComensales($ComensalInsertar);
                   	    redirect("comensales?save=true");
                   }
    			   if($VerifyExist>0){
                        $this->session->set_flashdata('msg', '<div class="alert alert-error text-center">Codigo Duplicado</div>');
                        $this->load->view('header');
    					$this->load->view('view_nuevo_comensal');
    					$this->load->view('footer');
                   }
    			
    		}else{
    			  $this->load->view('header');
    			  $this->load->view('view_nuevo_comensal');
    			  $this->load->view('footer');
    		} 
        }
        function ValidaCampos(){
    		/*Campos para validar que no esten vacio los campos*/
    		 $this->form_validation->set_rules("CODIGO", "Codigo", "trim|required");
    		 $this->form_validation->set_rules("NOMBRE", "Nombre", "trim|required");
    		 $this->form_validation->set_rules("APELLIDOS", "Apellidos", "trim|required");
    		 $this->form_validation->set_rules("SEXO", "Sexo", "callback_select_tipo");
    		 $this->form_validation->set_rules("DNI", "Dni", "trim|required");
    		 $this->form_validation->set_rules("FACULTAD", "Facultad", "trim|required");
    		 $this->form_validation->set_rules("EDAD", "Edad", "trim|required");
    		 $this->form_validation->set_rules("CORREO", "Correo", "trim|required");
    	 }
    }
    /* Archivo clientes.php */
    /* Location: ./application/controllers/clientes.php */

---
+++ application/controllers/comensales.php
@@ -0,0 +1,70 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+date_default_timezone_set('America/Mexico_City'); 
+class comensales extends CI_Controller
+{
+
+     public function __construct()
+     {
+          parent::__construct();
+          //Cargamos el modelo deel controlador
+          $this->load->model('model_usuarios');
+          $this->load->model('model_seguridad');
+          $this->load->model('model_login');
+          $this->load->model('model_comensales');
+     }
+     function Seguridad(){
+     	$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+         $this->model_seguridad->SessionActivo($url);
+     }
+     public function index(){
+          /*Si el usuario esta logeado*/
+          $this->Seguridad();
+          $this->load->view('header');
+          $data['comensales'] = $this->model_comensales->ListarComensales();         
+          $this->load->view('view_comensales', $data);
+          $this->load->view('footer');
+	}
+	public function nuevo(){
+	      
+        /*Si el usuario esta logeado*/
+        $this->Seguridad();
+		$hoy    = date("Y")."-".date("m")."-".date("d")." ".date("H:i:s");
+				
+		$this->ValidaCampos();
+		if($this->form_validation->run() == TRUE){
+				//Verificamos si existe el CODIGO
+			   $VerifyExist = $this->model_comensales->ExisteCodigo($this->input->post("CODIGO"));
+               if($VerifyExist==0){
+               	    $ComensalInsertar = $this->input->post();//Recibimos todo los campos por array nos lo envia codeigther
+               	    $ComensalInsertar["FECHA_REGISTRO"] = $hoy;//le agregamos la fecha de registro
+               	    //guardamos los registros
+               	    $this->model_comensales->SaveComensales($ComensalInsertar);
+               	    redirect("comensales?save=true");
+               }
+			   if($VerifyExist>0){
+                    $this->session->set_flashdata('msg', '<div class="alert alert-error text-center">Codigo Duplicado</div>');
+                    $this->load->view('header');
+					$this->load->view('view_nuevo_comensal');
+					$this->load->view('footer');
+               }
+			
+		}else{
+			  $this->load->view('header');
+			  $this->load->view('view_nuevo_comensal');
+			  $this->load->view('footer');
+		} 
+    }
+    function ValidaCampos(){
+		/*Campos para validar que no esten vacio los campos*/
+		 $this->form_validation->set_rules("CODIGO", "Codigo", "trim|required");
+		 $this->form_validation->set_rules("NOMBRE", "Nombre", "trim|required");
+		 $this->form_validation->set_rules("APELLIDOS", "Apellidos", "trim|required");
+		 $this->form_validation->set_rules("SEXO", "Sexo", "callback_select_tipo");
+		 $this->form_validation->set_rules("DNI", "Dni", "trim|required");
+		 $this->form_validation->set_rules("FACULTAD", "Facultad", "trim|required");
+		 $this->form_validation->set_rules("EDAD", "Edad", "trim|required");
+		 $this->form_validation->set_rules("CORREO", "Correo", "trim|required");
+	 }
+}
+/* Archivo clientes.php */
+/* Location: ./application/controllers/clientes.php */
