38e926c81d5c1261a5b0c6f76c9e2a4e94578f5d
RubenTorres Bazan <39686493+rubentb@users.noreply.github.com>
2018-06-02 22:25:56 -0500

    update view_comensales.php container
    <script type="text/javascript">
                /*CLIENTES*/
                $(document).ready(function() {
                    $('#comensales').dataTable( {
                        // sDom: hace un espacio entre la tabla y los controles 
                    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
            
                    } );
                } );
    </script>
    
    <div id="container">
    	<h2 align="center">CATALOGO DE COMENSALES</h2>
    	<?php
    if(isset($_GET['save'])){
    	echo '<div class="alert alert-success text-center">La Información  se Almaceno Correctamente</div>';
    }
    if(isset($_GET['delete'])){
    	echo '<div class="alert alert-warning text-center">La Información  se ha Eliminado Correctamente</div>';
    }
    if(isset($_GET['update'])){
    	echo '<div class="alert alert-success text-center">La Información  se Actualizo Correctamente</div>';
    }
    if(isset($_GET['permisos'])){
    		echo '<div class="alert alert-success text-center">Los Permisos fueron Asignados Correctamente</div>';
    	}
    	if(isset($_GET['password'])){
    		echo '<div class="alert alert-success text-center">La Contraseña fue actualizado Correctamente</div>';
    	}
    ?>
    <center>
    <table id="comensales" border="0" cellpadding="0" cellspacing="0" class="pretty">
    <thead>
    <tr>
    <th>ACCION</th>
    <th>CODIGO</th>
    <th>NOMBRES</th>
    <th>APELLIDOS</th>
    <th>ESCUELA PROFESIONAL</th>
    <th>EMAIL</th>
    <th>GENERO</th>
    <th>ESTADO</th>
    </tr>
    </thead>
    <tbody>

--- application/views/view_comensales.php
+++ application/views/view_comensales.php
@@ -1,1 +1,46 @@
+
 <script type="text/javascript">
+            /*CLIENTES*/
+            $(document).ready(function() {
+                $('#comensales').dataTable( {
+                    // sDom: hace un espacio entre la tabla y los controles 
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        
+                } );
+            } );
+</script>
+
+<div id="container">
+	<h2 align="center">CATALOGO DE COMENSALES</h2>
+	<?php
+if(isset($_GET['save'])){
+	echo '<div class="alert alert-success text-center">La Información  se Almaceno Correctamente</div>';
+}
+if(isset($_GET['delete'])){
+	echo '<div class="alert alert-warning text-center">La Información  se ha Eliminado Correctamente</div>';
+}
+if(isset($_GET['update'])){
+	echo '<div class="alert alert-success text-center">La Información  se Actualizo Correctamente</div>';
+}
+if(isset($_GET['permisos'])){
+		echo '<div class="alert alert-success text-center">Los Permisos fueron Asignados Correctamente</div>';
+	}
+	if(isset($_GET['password'])){
+		echo '<div class="alert alert-success text-center">La Contraseña fue actualizado Correctamente</div>';
+	}
+?>
+<center>
+<table id="comensales" border="0" cellpadding="0" cellspacing="0" class="pretty">
+<thead>
+<tr>
+<th>ACCION</th>
+<th>CODIGO</th>
+<th>NOMBRES</th>
+<th>APELLIDOS</th>
+<th>ESCUELA PROFESIONAL</th>
+<th>EMAIL</th>
+<th>GENERO</th>
+<th>ESTADO</th>
+</tr>
+</thead>
+<tbody>
